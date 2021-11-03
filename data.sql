fac55d65e68b556d33095b65d2837a20dcf814d7
Sabry Kadi <sabry.kadi@cybercom.com>
2021-11-03 15:23:29 +0100

    test1

---
+++ data.sql
@@ -0,0 +1,14 @@
+INSERT INTO  tmdb.movie (name)
+VALUES (
+	'shawshank redemption'
+);
+
+INSERT INTO tmdb.movie (name)
+VALUES (
+	'Spider-man'
+);
+
+INSERT INTO tmdb.movie (name)
+VALUES (
+	'Test movie'
+);
