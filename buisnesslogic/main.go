2117c33f5056178e8980e00f0a17a8e147cb7303
Son <son.nguyen@knowit.com>
2021-11-04 15:00:24 +0100

    Laderlappen

--- buisnesslogic/main.go
+++ buisnesslogic/main.go
@@ -18,7 +18,7 @@ func backend() {
 
 func setupResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Läderlappen", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
