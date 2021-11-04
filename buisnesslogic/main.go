ab0c8fffaec15c4002737a93eddd467164356522
Son <son.nguyen@knowit.com>
2021-11-04 14:57:55 +0100

    Batman

--- buisnesslogic/main.go
+++ buisnesslogic/main.go
@@ -18,7 +18,7 @@ func backend() {
 
 func setupResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Banankontakt", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("BatmanLäderlappen", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
